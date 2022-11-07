82cf4f68c846a27589cd4d7b2593250dbcda5e05
Sheikh Fahad <116126902+WinonaStateCSClub@users.noreply.github.com>
2022-11-06 18:18:34 -0600

    Template Created

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/WinonaStateCSClub",
+    "name": "Winona State CS Club",
+    "role": "just a Winona State Club",
+    "linkedinId":"https://www.linkedin.com/",
+    "githubId": "http://github.com/",
+    "roleDescription": "We like to do HW Help and pizza when we have nothing going on.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        { 
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I have been a WSU club for 30 years",
+    "address":"Winona State",
+    "website":"http://github.com/",
+    "education":[
+      {
+        "UniversityName":"Winona State University",
+        "specialization":"Computer Science and Data Science Major",
+        "MonthOfPassing":"Spring",
+        "YearOfPassing":"2023",
+        "Achievements":"President of CS Club"
+      },
+      {
+        "UniversityName":"Harvard",
+        "specialization":"Biochemical Engineering",
+        "MonthOfPassing":"Spring",
+        "YearOfPassing":"2025",
+        "Achievements":"I initiated the war with other worldly androids"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"WSU",
+        "specialization":"Club",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"1998",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"web dev",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"web dev",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"web dev",  
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"web dev",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      }
+    ],
+    "recommendations":[
+      {
+        "description":"People who recommended me",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"People who recommended me",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
